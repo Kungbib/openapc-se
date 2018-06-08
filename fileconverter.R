6cf51dea454dc41f5cf4ab12b7fbd131a0ec4499
Camilla Lindelow <camilla.lindelow@kb.se>
2018-06-08 11:01:37 +0200

    added oru data

--- fileconverter.R
+++ fileconverter.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 
 converter <- read_xlsx("data/oru/original_data/ORU APC_data 2015_2017_upload 20180601.xlsx")
-# converter <- select(converter, 1:5)
-write_tsv(converter, "data/oru/apc_oru_2015_2017.tsv", na = "")
+converter_dois <- filter(converter, !(is.na(doi)))
+converter_non_dois <- filter(converter, (is.na(doi)))
+write_tsv(converter_dois, "data/oru/original_data/apc_oru_2015_2017_dois.tsv", na = "")
+write_tsv(converter_non_dois, "data/oru/original_data/apc_oru_2015_2017_non_dois.tsv", na = "")
 
-test <- read_csv("data/oru/apc_oru_2015_2017.tsv")
