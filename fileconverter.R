5a1bea69deefa595585b10c8b993ce147f2ad67e
Camilla Lindelow <camilla.lindelow@kb.se>
2018-06-19 13:05:50 +0200

    added kth data 2017

--- fileconverter.R
+++ fileconverter.R
@@ -1,8 +1,9 @@
 library(tidyverse)
+library(readxl)
 
-converter <- read_xlsx("data/oru/original_data/ORU APC_data 2015_2017_upload 20180601.xlsx")
+converter <- read_xlsx("data/kth/original_data/apc_kth_2017.xlsx")
 converter_dois <- filter(converter, !(is.na(doi)))
 converter_non_dois <- filter(converter, (is.na(doi)))
-write_tsv(converter_dois, "data/oru/original_data/apc_oru_2015_2017_dois.tsv", na = "")
-write_tsv(converter_non_dois, "data/oru/original_data/apc_oru_2015_2017_non_dois.tsv", na = "")
+write_tsv(converter_dois, "data/kth/original_data/apc_kth_2017_dois.tsv", na = "")
+write_tsv(converter_non_dois, "data/kth/original_data/apc_kth_2017_non_dois.tsv", na = "")
 
