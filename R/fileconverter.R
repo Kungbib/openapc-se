dc1996b7ea53d774d26dc6945669feec5c0a59e6
camillalindelow <camilla.lindelow@kb.se>
2018-11-26 15:40:54 +0100

    added uu-data

--- R/fileconverter.R
+++ R/fileconverter.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(readxl)
 
 # what organisation, short name? ex kth
-organisation <- 'lnu'
+organisation <- 'uu'
 
 # data collected from which timeperiod? ex 2010-2019
 timeperiod_data <- '2017'
 
 # what's the name of the file to be converted?
-indata_file <- 'data/lnu/original_data/APCSweden_Lnu.xlsx'
+indata_file <- 'data/uu/original_data/UUAPCSweden2017.xlsx'
 
 
 outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.tsv')
