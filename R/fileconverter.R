f78ca1c919c6e53a2753eb99a9bb23d808ed68bc
camillalindelow <camilla.lindelow@kb.se>
2019-02-26 15:26:33 +0100

    added du and kth-data

--- R/fileconverter.R
+++ R/fileconverter.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(readxl)
 
 # what organisation, short name? ex kth
-organisation <- 'uu'
+organisation <- 'kth'
 
 # data collected from which timeperiod? ex 2010-2019
 timeperiod_data <- '2017'
 
 # what's the name of the file to be converted?
-indata_file <- 'data/uu/original_data/UUAPCSweden2017.xlsx'
+indata_file <- 'data/kth/original_data/2017_OpenAPC_Sweden_Agresso_table.xlsx'
 
 
 outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.tsv')
