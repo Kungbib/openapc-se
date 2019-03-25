8354e432bf040b428903624924e6cb11cf6ddbee
camillalindelow <camilla.lindelow@kb.se>
2019-03-25 11:50:52 +0100

    added Springer Compact full data for 2016-2018 prepared new apc-file without SC-publs

--- R/datafile_sorter.R
+++ R/datafile_sorter.R
@@ -5,10 +5,10 @@
 library(tidyverse)
 
 apc_se <- read_csv("data/apc_se.csv")
-springer_dois <- read_tsv("data/springer/springer_doi_list.tsv")
+springer_dois <- read_tsv("data/springer/Springer_Compact_2016_2018.tsv")
 
 springer_data <- left_join(springer_dois, apc_se, by="doi")
 apc_se_without_springer <- anti_join(apc_se, springer_dois, by="doi")
 
-write_tsv(springer_data, "data/springer_data.tsv") 
-write_tsv(apc_se_without_springer, "data/apc_se_without_springer.tsv") 
+write_tsv(springer_data, "data/springer_data_1.tsv") 
+write_tsv(apc_se_without_springer, "data/apc_se_without_springer_1.tsv") 
