f6eb9ee01ea82c8a325744e2bf191f2967830e3b
camillalindelow <camilla.lindelow@kb.se>
2019-07-18 15:28:23 +0200

    added su 2018 data

--- R/fileconverter.R
+++ R/fileconverter.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(readxl)
 
 # what organisation, short name? ex kth
-organisation <- 'gih'
+organisation <- 'su'
 
 # data collected from which timeperiod? ex 2010-2019
 timeperiod_data <- '2018'
 
 # what's the name of the file to be converted?
-indata_file <- 'data/gih/original_data/gih-2018.xlsx'
+indata_file <- 'data/su/original_data/apc_su_2018.xlsx'
 
 
 outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.tsv')
