86ece891e714cfb26b5b5252e5280fa7b470bb16
camillalindelow <camilla.lindelow@kb.se>
2019-12-30 12:37:02 +0100

    added lnu 2018 data

--- R/fileconverter.R
+++ R/fileconverter.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(readxl)
 
 # what organisation, short name? ex kth
-organisation <- 'sh'
+organisation <- 'lnu'
 
 # data collected from which timeperiod? ex 2010-2019
 timeperiod_data <- '2018'
 
 # what's the name of the file to be converted?
-indata_file <- 'data/sh/original_data/open_apc_sh_2018.xlsx'
+indata_file <- 'data/lnu/original_data/bearbetad_2018_APCSweden_Lnu.xlsx'
 
 
 outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.tsv')
