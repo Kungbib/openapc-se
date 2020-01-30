8c50a7fff506744d7cc8ecd66488863cb91535e2
camillalindelow <camilla.lindelow@kb.se>
2020-01-30 14:09:50 +0100

    added kth 2018 data

--- R/fileconverter.R
+++ R/fileconverter.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(readxl)
 
 # what organisation, short name? ex kth
-organisation <- 'mdh'
+organisation <- 'kth'
 
 # data collected from which timeperiod? ex 2010-2019
-timeperiod_data <- '2019'
+timeperiod_data <- '2018'
 
 # what's the name of the file to be converted?
-indata_file <- 'data/mdh/original_data/mdh_lev1_2019.xlsx'
+indata_file <- 'data/kth/original_data/2018_KTH.xlsx'
 
 
 outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.tsv')
@@ -17,6 +17,11 @@ outdata_file_non_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',
 
 converter <- read_xlsx(indata_file)
 # converter <- read_csv(indata_file)
+
+converter <- converter %>%
+    mutate(euro = 0.0944*sek) %>% #valutakurs hämtad från https://www.riksbank.se/sv/statistik/sok-rantor--valutakurser/arsgenomsnitt-valutakurser/?y=2019&m=12&s=Comma&f=y
+    select(-sek) %>%
+    select(institution, period, euro, doi, is_hybrid, publisher, journal_full_title, issn, issn_print, issn_electronic, url)
 
 converter_dois <- converter %>%
     filter(!(is.na(doi))) %>%
