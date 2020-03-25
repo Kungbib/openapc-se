73caedfa37a77cbd80fea2f8acba0d54126552df
camillalindelow <camilla.lindelow@kb.se>
2020-03-25 17:17:47 +0100

    added 2019 data for du, his and liu

--- R/fileconverter.R
+++ R/fileconverter.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(readxl)
 
 # what organisation, short name? ex kth
-organisation <- 'liu'
+organisation <- 'du'
 
 # data collected from which timeperiod? ex 2010-2019
-timeperiod_data <- '2019_jan_jun'
+timeperiod_data <- '2019'
 
 # what's the name of the file to be converted?
-indata_file <- 'data/liu/original_data/APC_LiU_jan-jun_2019.xlsx'
+indata_file <- 'data/du/original_data/Open_APC_Sweden-DU_2019_2020-03-25.xlsx'
 
 
 outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.tsv')
