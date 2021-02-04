d32844b08303f0bf5f844229e71691e59567d12e
camillalindelow <camilla.lindelow@kb.se>
2021-02-04 12:57:37 +0100

    added KI data 2020 Q1 & Q2. Minor corrections other data.

--- R/fileconverter.R
+++ R/fileconverter.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(readxl)
 
 # what organisation, short name? ex kth
-organisation <- 'ltu'
+organisation <- 'ki'
 
 # data collected from which timeperiod? ex 2010-2019
-timeperiod_data <- '2019'
+timeperiod_data <- '2020_Q1_rest'
 
 # what's the name of the file to be converted?
-indata_file <- 'data/ltu/original_data/ltu_2019.xlsx'
+indata_file <- 'data/ki/original_data/KI Q1 2020_rest.xlsx'
 
 
 outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.tsv')
@@ -19,7 +19,7 @@ converter <- read_xlsx(indata_file)
 # converter <- read_csv(indata_file)
 
 converter <- converter %>%
-    mutate(euro = 0.0944*sek) %>% #valutakurs hämtad från https://www.riksbank.se/sv/statistik/sok-rantor--valutakurser/arsgenomsnitt-valutakurser/?y=2019&m=12&s=Comma&f=y
+    mutate(euro = 0.0938*sek) %>% #valutakurs hämtad från https://www.riksbank.se/sv/statistik/sok-rantor--valutakurser/arsgenomsnitt-valutakurser/?y=2019&m=12&s=Comma&f=y
     select(-sek) %>%
     select(institution, period, euro, doi, is_hybrid, publisher, journal_full_title, issn, issn_print, issn_electronic, url)
 
--- data/kth/readme.md
+++ data/kth/readme.md
@@ -1,3 +1,6 @@
-APC data files from KTH Royal Institute of Technology are published here.
+APC data files from KI are published here.
 
-[Updated 2017-01-11/UK]
+KI delivers data quarterly, and currency exchange is based on a quarterly mean obtained from the Swedish riksbank:
+https://www.riksbank.se/sv/statistik/sok-rantor--valutakurser/arsgenomsnitt-valutakurser/
+
+[Updated 2021-02-03/CL]
