a868813f85cfdf3c1875f83278395500567acec8
JohanCFroberg <johan.froberg@kb.se>
2024-02-15 13:54:42 +0100

    added kth and liu 2023 data

--- R/fileconverter.R
+++ R/fileconverter.R
@@ -3,17 +3,35 @@
 library(tidyverse)
 library(readxl)
 
+# clean environment when doing several consecutive runs, keep bibsam- and openapc-data
+rm(check_bibsam, check_initiative, converter, doi_check, doi_dubbletter,
+   for_sending_to_initiative, with_dois, with_dois_checked, without_dois,
+   check_bibsam_file, check_initiative_file, indata_file, outdata_file, 
+   organisation, timeperiod_data, check_org_period, indata, high_apcs, check_column_names)
+
+# definitions of column names and types
+column_names <- c("institution", "period", "sek", "doi", "is_hybrid", "publisher", "journal_full_title", "issn", "issn_print", "issn_electronic", "url")
+column_types <- c("text", "numeric", "numeric", "text", "logical", "text", "text", "text", "text", "text", "text")
 
 # settings: change before running -----------------------------------------
 
 # what organisation, short name? ex kth
-organisation <- 'hig'
+organisation <- 'kth'
 
 # data collected from which timeperiod? ex 2010-2019, 2020_Q1
 timeperiod_data <- '2023'
 
-# what's the name of the file to be converted?
-indata_file <- str_c('data/', organisation, '/original_data/HIG_APC_2023.xlsx')
+# what's the name of the file or files to be converted?
+indata_file <- str_c('data/', organisation, '/original_data/apc_kth_2023.xlsx')
+
+# indata_file2 <- str_c('data/', organisation, '/original_data/apc_liu_ht2023.xlsx')
+
+# outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.csv')
+# outdata_file_non_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_non_dois.csv')
+outdata_file <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'.csv')
+check_bibsam_file <- str_c('data/',organisation,'/','check_bibsam_',organisation,'_',timeperiod_data,'.csv')
+check_initiative_file <- str_c('data/',organisation,'/','check_initiative_',organisation,'_',timeperiod_data,'.csv')
+
 
 # tu_file <- tibble(
 #   institution = character(),
@@ -23,27 +41,59 @@ indata_file <- str_c('data/', organisation, '/original_data/HIG_APC_2023.xlsx')
 #   is_hybrid = logical()
 # )
 
+# check of incoming file -------------------------------------------------
+# code to represent check list in Handbok_openapcsweden
 
-# outdata_file_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_dois.csv')
-# outdata_file_non_dois <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'_non_dois.csv')
-outdata_file <- str_c('data/',organisation,'/','apc_',organisation,'_',timeperiod_data,'.csv')
-check_bibsam_file <- str_c('data/',organisation,'/','check_bibsam_',organisation,'_',timeperiod_data,'.csv')
-check_initiative_file <- str_c('data/',organisation,'/','check_initiative_',organisation,'_',timeperiod_data,'.csv')
+# reads indata file, gives error if number of columns are incorrect, if so add missing columns in excel
+indata <- read_xlsx(indata_file, col_types = column_types)
 
+# if multiple indata fiels
+indata <- bind_rows(read_xlsx(indata_file, col_types = column_types), read_xlsx(indata_file2, col_types = column_types))
+
+# check column names creates character string with wrong names
+check_column_names <- setdiff(colnames(indata), column_names) 
+
+# if wrong names, rename all column names to correct
+indata <- rename_with(indata, ~ column_names)
+
+# check organisation and period
+check_org_period <- filter(indata, period != timeperiod_data | institution != organisation)
+
+# set correct organisation and period
+indata <- mutate(indata,
+                      institution = organisation, 
+                      period = timeperiod_data)
+
+# find spaces in doi
+doi_check <- subset(indata, str_detect(doi, "[\\s]"))
+# remove spaces in doi and change doi to right format
+indata <- mutate(indata, 
+                 # doi = str_replace(doi, "https://", ""),
+                 doi = str_replace_all(doi, "[\\s]", ""),
+                 doi = if_else(str_starts(doi, "10."), doi, str_replace(doi, "^.*(?=10.*)", ""))
+                 )
+
+
+doi_check <- subset(indata, str_detect(doi, "[\\s]"))
+
+# find doi duplicates, if != 0 resolve with organisation alter and start over 
+doi_dubbletter <- subset(indata, duplicated(doi)) # hittar doi_dubbletter
+
+# find high apc:s, if any resolve with organisation
+high_apcs <- filter(indata, sek > 80000)
 
 # conversion --------------------------------------------------------------
-column_types <- c("text", "numeric", "numeric", "text", "logical", "text", "text", "text", "text", "text", "text")
-converter <- read_xlsx(indata_file, col_types = column_types)
-# converter <- read_csv(indata_file)
-doi_check <- subset(converter, str_detect(doi, "[\\s]")) # hittar mellanslag i doi
-doi_dubbletter <- subset(converter, duplicated(doi)) # hittar doi_dubbletter
+
+# converter <- read_xlsx(indata_file2, col_types = column_types)
+
 # kom ihåg att KI är ett år före, och kommer leverera data under innevarande år, därav
 # en egen rad för dem.
-converter <- converter %>%
+converter <- indata %>%
   # standard:
   mutate(euro = format(round(0.0871*sek, 2), nsmall = 2)) %>% 
   # valutakurs 2023 hämtad från 
   # https://www.riksbank.se/sv/statistik/sok-rantor--valutakurser/arsgenomsnitt-valutakurser/
+  
   # KI (kvartalsvisa medelvärden):
   # mutate(euro = format(round(0.0937*sek, 2), nsmall = 2)) %>% #valutakurs 2022 q2-q4 hämtad från https://www.riksbank.se/sv/statistik/sok-rantor--valutakurser/ (månadsgenomsnitt)    select(-sek) %>%
   select(-sek) %>%
@@ -57,7 +107,8 @@ without_dois <- filter(converter, is.na(doi))
 with_dois <- filter(converter, !(is.na(doi)))
 
 # tvätta mot Bibsams publiceringsdata (se till att använda uppdaterad fil)
-# bibsam_data <- read_csv("data/19_21_bibsam_data.csv") ändrat till att läsa filen från GitHUB
+# bibsam_data <- read_csv("data/19_21_bibsam_data.csv") 
+# ändrat till att läsa filen från GitHUB
 bibsam_data <- read_csv("https://raw.githubusercontent.com/Kungbib/oa-tskr/master/Bibsam_artikeldata/19_22_bibsam_data.csv")
 
 with_dois_checked <- anti_join(with_dois, bibsam_data, by = "doi")
