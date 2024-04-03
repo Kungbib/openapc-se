e7c636d9a536a192087f54146be449114fbcd3ef
JohanCFroberg <johan.froberg@kb.se>
2024-04-03 10:25:01 +0200

    added 2023 lu bookpc data

--- R/book_file_converter.R
+++ R/book_file_converter.R
@@ -8,22 +8,24 @@ column_names <- c("institution", "period", "sek", "doi", "backlist_oa", "publish
 column_types <- c("text", "numeric", "numeric", "text", "logical", "text", "text", "text", "text", "text")
 
 # rm between consecutive runs
-rm(organisation, timeperiod_data, indata_file, outdata_file, check_initiative_file, indata,
-   check_column_names, doi_check, doi_dubbletter, converter, check_initiative, for_sending_to_initiative)
+rm(organisation, timeperiod_data, indata_file, outdata_file, check_initiative_file, missing_doi_isbn_file, indata,
+   missing_doi_isbn, check_column_names, doi_check, doi_dubbletter_all, isbn_dubbletter_all, 
+   converter, check_initiative, for_sending_to_initiative)
 
 # settings: change before running -----------------------------------------
 
 # what organisation, short name? ex kth
-organisation <- 'lnu'
+organisation <- 'lu'
 
 # data collected from which timeperiod? ex 2010-2019, 2020_Q1
 timeperiod_data <- '2023'
 
 # what's the name of the file to be converted?
-indata_file <- str_c('data/', organisation, '/original_data/2023 OpenBPC Lnu.xlsx')
+indata_file <- str_c('data/', organisation, '/original_data/lu_bpc_2023.xlsx')
 
 outdata_file <- str_c('data/', organisation, '/bookpc_', organisation, '_', timeperiod_data, '.csv')
 check_initiative_file <- str_c('data/',organisation,'/','book_check_initiative_',organisation,'_',timeperiod_data,'.csv')
+missing_doi_isbn_file <- str_c('data/', organisation, "/missing_doi_and_isbn_", organisation, "_", timeperiod_data, ".csv" )
 
 
 # conversion --------------------------------------------------------------
@@ -35,15 +37,30 @@ check_column_names <- setdiff(colnames(indata), column_names)
 # if wrong names, rename all column names to correct
 indata <- rename_with(indata, ~ column_names)
 
+# remove extra column(s) and rows which have no doi or isbn, which first are written to separate table
+indata <- select(indata, -url) 
+missing_doi_isbn <- filter(indata, is.na(doi) & is.na(isbn_1))
+indata <- filter(indata, !is.na(doi) | !is.na(isbn_1))
+
 doi_check <- subset(indata, str_detect(doi, "[\\s]")) # hittar mellanslag i doi
 
 indata <- mutate(indata, 
                  # doi = str_replace(doi, "https://", ""),
                  doi = str_replace_all(doi, "[\\s]", ""),
-                 doi = if_else(str_starts(doi, "10."), doi, str_replace(doi, "^.*(?=10.*)", ""))
+                 doi = if_else(str_starts(doi, "10."), doi, str_replace(doi, "^.*(?=10.*)", "")),
+                 isbn_1 = str_replace_all(isbn_1, "-", "")
 )
 
-doi_dubbletter <- subset(indata, duplicated(doi)) # hittar doi_dubbletter
+# hittar doi_dubbletter
+doi_dubbletter_all <- group_by(indata, doi) %>% 
+    filter(n() > 1) %>% 
+    ungroup() %>% 
+    arrange(doi) 
+
+isbn_dubbletter_all <- group_by(indata, isbn_1) %>% 
+    filter(n() > 1) %>% 
+    ungroup() %>% 
+    arrange(doi)
 
 converter <- indata %>%
   # standard:
@@ -66,3 +83,4 @@ for_sending_to_initiative <- anti_join(converter, check_initiative, by = "doi")
 
 write_csv(for_sending_to_initiative, outdata_file, na = '')
 if (nrow(check_initiative) > 0) write_csv(check_initiative, check_initiative_file, na = '')
+if (nrow(missing_doi_isbn) > 0) write_csv(missing_doi_isbn, missing_doi_isbn_file, na = '')
