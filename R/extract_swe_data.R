24d4f5a597dcda36f007bc71a1059a9186dab67e
JohanCFroberg <johan.froberg@kb.se>
2024-04-23 17:47:41 +0200

    updated apc_swe.csv with 2023 data

--- R/extract_swe_data.R
+++ R/extract_swe_data.R
@@ -18,7 +18,8 @@ de_data <- read_csv("https://raw.githubusercontent.com/OpenAPC/openapc-de/master
 org_help_table <- read_xlsx(config$org_help_file[1])
 
 # filter SWE data
-swe_data <- filter(de_data, institution %in% org_help_table$open_apc_name)
+swe_data <- filter(de_data, institution %in% org_help_table$open_apc_name) %>% 
+    arrange(institution, period)
 
 # create test table compare institutions
 summary_swe_data <- filter(swe_data, period == year_to_analyse) %>% 
