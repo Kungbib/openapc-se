401f4f4cff54aa67e1188ac22e7dc421978bada5
JohanCFroberg <johan.froberg@kb.se>
2026-03-27 13:44:19 +0100

    changes for OpenAPC workshop

--- R/extract_swe_data.R
+++ R/extract_swe_data.R
@@ -12,6 +12,10 @@ year_to_analyse = 2024
 # read data from Open APC DE
 de_data <- read_csv("https://raw.githubusercontent.com/OpenAPC/openapc-de/master/data/apc_de.csv")
 
+currency_converter <- read_xlsx("~/r/repos/normalisera_forlagsdata/admin_files/currency_converter.xlsx") %>% 
+    filter(currency == "EUR") %>% 
+    select(-c("currency", "conversion_to"))
+
 # org_in_de_data <- select(de_data, institution) %>% distinct()
 
 # read organisations from Forskningssamverkansstatistik/help_files
@@ -30,3 +34,21 @@ write_csv(swe_data, "data/apc_swe.csv")
 
 # write Excel-fil for Bibsam on Forskningssamverkansstatistik
 write.xlsx(swe_data, config$xlsx_file[1])
+
+
+# prep OpenAPC workshop ---------------------------------------------------
+
+sum_over_years <- filter(swe_data, period > 2016) %>% 
+    anti_join(bibsam_data, by = "doi") %>% 
+    group_by(period, is_hybrid) %>% 
+    summarize(n = n(),
+              amount = sum(euro)) %>% 
+    left_join(currency_converter, by = c("period" = "year")) %>% 
+    mutate(amount_sek = amount * conversion)
+
+write.xlsx(sum_over_years, "temp/workshop_oct_2025.xlsx")
+
+ggplot(filter(sum_over_years), aes(x = period)) + 
+    geom_line(aes(y = n))
+ 
+             
